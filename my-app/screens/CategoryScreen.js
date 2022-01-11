c26a855adc93238e14c101ebfe66ceca93034113
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-01-11 17:40:16 +0200

    Add files via upload

---
+++ my-app/screens/CategoryScreen.js
@@ -0,0 +1,44 @@
+import { View,  FlatList,Text } from 'react-native';
+import styles from '../assets/Style';
+// import ProductGridTile from './../components/ProductGridTile';
+import { CATEGORIES, PRODUCTS } from '../data/data';
+
+export default function CategoryScreen({ route, navigation }) {
+
+  const categoryID = route.params.categoryID
+
+  // const catTitle = CATEGORIES.find(cat => cat.id === categoryID).title
+
+  const displayedProducts = PRODUCTS.filter(item => item.catID == categoryID)
+
+  const renderGridItem = ({ item }) => {
+    return (
+      // TODO: change this
+      <ProductGridTile
+        imageUrl={item.imageUrl}
+        title={item.title}
+        price={item.price}
+        onSelect={() => {
+          navigation.navigate("ProductDetail", {
+            productID: item.id,
+            productTitle: item.title
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.BG}>
+      {/* <View style={{padding:120}}>
+        <FlatList
+          data={displayedProducts}
+          keyExtractor={(item) => item.id}
+          renderItem={renderGridItem}
+          numColumns={2}
+        />
+      </View> */}
+      <Text>asdsadd</Text>
+    </View>
+  );
+}
