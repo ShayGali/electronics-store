91254fe61e8cb97dd5a0e68c919e8f91c1eb9e81
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-11 23:58:56 +0200

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import AppNavigator from "./navigation/AppNavigation";
 import { StyleSheet, Text, View, Button } from "react-native";
+import * as Font from "expo-font"; // import font
+import AppLoading from "expo-app-loading";
+
+
+ const fetchFonts = () => {
+   Font.loadAsync({
+     "oswald": require("./assets/fonts/Oswald,Reggae_One/Oswald/Oswald-VariableFont_wght.ttf"),
+     "reggae-One": require("./assets/fonts/Oswald,Reggae_One/Reggae_One/ReggaeOne-Regular.ttf"),
+   });
+ };
 
 export default function App() {
+  const[dataLoaded, setDataLoaded] = useState(false);
+
+  if(!dataLoaded){
+    return(
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={() => console.log(err)}
+        />
+    );
+  }
+
   return (
     <AppNavigator/>
   );
