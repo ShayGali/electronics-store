fbbd8f1375df7c25eef14fa4f880d3b2d228c1c1
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-12 16:57:10 +0200

    Update ProductDetail.js

--- my-app/screens/ProductDetail.js
+++ my-app/screens/ProductDetail.js
@@ -1,43 +1,46 @@
-import { View, Text, Image,TouchableOpacity,ScrollView ,Divider,Alert  } from 'react-native';
+import { View, Text, Image,TouchableOpacity,ScrollView ,Divider } from 'react-native';
 import styles from '../assets/Style';
 import { PRODUCTS } from '../data/data';
 import { MaterialCommunityIcons as Icon } from "react-native-vector-icons";
-// import { IconFill, IconOutline } from "@ant-design/icons-react-native";
+import ImageResizeMode from 'react-native/Libraries/Image/ImageResizeMode'
+//import { ShoppingCartOutlined } from '@ant-design/icons'
 // import { Divider } from 'react-native-elements'
 
-
-export default function ProductsDetailScreen({ route, navigation }) {
+export default function ProductDetail({ route, navigation }) {
 
   const productID = route.params.productID
 
   const currentProduct = PRODUCTS.find(product => product.id === productID)
-  // let a = {a:"Asd",b:"asdsad"}
   const image = { uri: currentProduct.imgUrl }
 
   return (
     <View style={styles.BG}>
       <ScrollView>
         <View style={{ margin: 50 }}>
-          <View>
-            <Image style={styles.prodImg} source={image} />
-          </View>
            <Text style={[styles.mainHL, { marginBottom: 20 }]}>{currentProduct.title}</Text>
 
+          <View style={[styles.viewProductImg]}>
+            <Image style={styles.prodImg} source={image} resizeMode='contain' />
+          </View>
 
           <Text style={[styles.headLines, { marginBottom: 10 }]}>Description</Text>
+          
           <View style={{ marginBottom: 30 }}>
             <Text style={styles.regText}>{currentProduct.description}</Text>
           </View>
+
           <View style={[styles.boxFrame, { marginBottom: 30 }]}>
             <Text style={[styles.regText, { margin: 5 }]}>Dimensions: {currentProduct.dimensions}</Text>
             <Text style={[styles.regText, { margin: 5 }]}>Price: {currentProduct.price}$</Text>
             <Text style={[styles.regText, { margin: 5 }]}>Shipping price: {currentProduct.shippingPrice}$</Text>
             <Text style={[styles.regText, { margin: 5 }]}>Final price: {currentProduct.price + currentProduct.shippingPrice}$</Text>
           </View>
-          <View style={[styles.boxFrame, { marginBottom: 30 }]}>
+
+          <View style={[styles.boxFrame, { marginBottom: 30, flexDirection: 'row' }]}>
             <Text style={[styles.headLines]}>Reviews:  </Text>
             <Text style={[styles.regText, styles.boxFrame]}>{currentProduct.reviews.calcGrade()} / 5</Text>
           </View>
+
           <View style={{ flexDirection: "row" }}>
             <Text style={[styles.regText, { marginBottom: 5 }]}>Add to cart -{'>'}  </Text>
 
