0891f4aec5131f2616f3aa80ba9fcffff8c70751
ShayG0 <86621168+ShayG0@users.noreply.github.com>
2022-01-12 15:19:04 +0200

    Update Review.js

--- my-app/models/Review.js
+++ my-app/models/Review.js
@@ -4,5 +4,13 @@ export default class Review {
         this.review = [fiveStars, fourStars, threeStars, twoStars, oneStar]
 
     }
+    calcGrade(){
+        let sum = 0;
+        for (let index = 0; index < this.review.length; index++) {
+            sum += this.review[index] * index+1;
+        }
+        let numOfReviews = this.review.reduce((a,b)=> a+b,0);
+        return sum/numOfReviews;
+    }
 
 }
