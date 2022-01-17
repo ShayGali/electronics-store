dcdd8ac21889c3553836acbfd3ff58de07ac9451
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-17 15:07:27 +0200

    Update Categories.js

--- my-app/models/Categories.js
+++ my-app/models/Categories.js
@@ -1,9 +1,10 @@
 export default class Category {
 
-    constructor(id, title, imgUrl){
+    constructor(id, title, imgUrl, amount){
         this.id = id;
         this.title = title;
         this.imgUrl = imgUrl;
+        this.amount = amount;
     }
 
 }
