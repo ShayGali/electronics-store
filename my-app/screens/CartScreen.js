b9e528332ebd67f99c6022999cdecb3cc3017f7d
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-18 19:15:12 +0200

    Update CartScreen.js

--- my-app/screens/CartScreen.js
+++ my-app/screens/CartScreen.js
@@ -1,16 +1,91 @@
-import React from "react";
-import { Text, View, Button } from "react-native";
-import { PRODUCTS, cart } from "../data/data";
+import React, { useState } from "react";
+import { Text, View, Button,TextInput, FlatList,ImageBackground } from "react-native";
+import { PRODUCTS, cart, COUPONS } from "../data/data";
+import styles from '../assets/Style';
 
-export default function CartScreen({ navigation }) {
+export default function CartScreen({ route, navigation }) {
+
+  const productsInCart = cart;
+
+  const price = cart.reduce(function (prev, current){
+    return prev + +current.price + +current.shippingPrice
+  }, 0);
+
+  let[priceDiscount, setPriceDiscount] = useState(+price);
+  let [myCoupon, setCoupon] = useState("");
+  let [isCouponValid, setIsCouponValid] = useState(true);
+
+  const cExists = COUPONS.find(item => item.coupon == myCoupon);
+  
+  let isValid = true;
+  const checkIfExists = () => {
+    if (cExists != undefined) {
+      setIsCouponValid(true);
+      isValid = true;
+      let discount = +cExists.discountPercentages;
+      setPriceDiscount( +priceDiscount * (1 - (+discount)));
+      return priceDiscount;
+    }else {
+      setIsCouponValid(false); 
+      return priceDiscount;
+    }
+};
+
+
+  const renderGridItem = ({ item }) => {
+    return (
+    <View style={styles.gridItem}> 
+
+      <ImageBackground imageStyle={{borderRadius: 25, opacity: 0.6}} source={{uri:item.imgUrl}} resizeMode='contain' style={styles.BGImg}>
+        <View style={[styles.gridContainer]} >
+          <Text style={[styles.titles, {fontSize: 28}]}>{item.title}</Text>
+          <Text style={[styles.titles, {fontSize: 35}]}>{item.price}$</Text>
+        </View>
+      </ImageBackground>
+      
+    </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Cart</Text>
-      <Text>{cart.map((product) => product.id)}</Text>
-      <Button
+    <View style={styles.BG}>
+        <Text style={[styles.titles,{fontSize:50}]}>Cart</Text>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={productsInCart}
+          renderItem={renderGridItem}
+          numColumns={1}
+        />
+      <View>
+        <Text style={styles.titles}>Final price: {price}$</Text>
+        
+        <View style={[styles.inputView]}>
+          <Text style={styles.textInput}>Coupon: </Text>
+          <TextInput
+            onChangeText={(text) => setCoupon(text)}
+            style={[styles.input, {marginBottom: 10}, isCouponValid ? "" : styles.inputError]}
+          />
+          <Text style={styles.errorMsg}>
+            {isCouponValid ? "" : "There is no such coupon..."}
+          </Text>
+
+          <Button
+          title="Submit"
+          style={styles.couponButton}
+          onPress={() => checkIfExists()}
+        />
+        </View>
+          <Text style={styles.titles}>Price after coupon discount: {priceDiscount}$</Text>
+
+        <Button
         title="Go to Payment..."
-        onPress={() => navigation.navigate("Payment")}
+        onPress={() => navigation.navigate("Payment", {
+          finalPrice: priceDiscount
+        })}
       />
+      </View>
     </View>
   );
+
 }
+
