31acccc34b9aa53c98106a994062a9ea3c24096a
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-19 19:33:28 +0200

    Update OrderedScreen.js

--- my-app/screens/OrderedScreen.js
+++ my-app/screens/OrderedScreen.js
@@ -1,13 +1,23 @@
 import { Text, View, Button } from "react-native";
+import styles from '../assets/Style';
 
 export default function OrderedScreen({ route, navigation }) {
+  let a = route.params.address;
+  let ci = route.params.city;
+  let co = route.params.country;
+
   return (
-    <View>
-      <Text>Thank you for your order</Text>
-      <Button
-        title="Go to Home Screen..."
-        onPress={() => navigation.navigate("HomeScreen")}
-      />
+    <View style={styles.BG}>
+      <View style={{padding: 20, textAlign: "center", justifyContent: "center"}}>
+        <Text style={[styles.mainHL, {margin: 10}]}>Thank you for your order!</Text>
+        <Text style={[styles.titles, {margin: 10}]}>Your order will be shipped to your address within 7 to 14 days...</Text>
+        <Text style={[styles.titles, styles.boxFrame, {margin: 10, marginBottom: 30}]}>Your address: {a}, {ci}, {co}</Text>
+        
+        <Button
+          title="Go to Home Screen..."
+          onPress={() => navigation.navigate("HomeScreen")}
+        />
+      </View>
     </View>
   );
 }
