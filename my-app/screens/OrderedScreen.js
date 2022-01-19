4771baccf1221e8df0e9cddd287664934a20806d
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-01-19 20:40:59 +0200

    Update OrderedScreen.js

--- my-app/screens/OrderedScreen.js
+++ my-app/screens/OrderedScreen.js
@@ -8,7 +8,7 @@ export default function OrderedScreen({ route, navigation }) {
 
   return (
     <View style={styles.BG}>
-      <View style={{padding: 20, textAlign: "center", justifyContent: "center"}}>
+      <View style={{padding: 20, textAlign: "center", alignContent: "center"}}>
         <Text style={[styles.mainHL, {margin: 10}]}>Thank you for your order!</Text>
         <Text style={[styles.titles, {margin: 10}]}>Your order will be shipped to your address within 7 to 14 days...</Text>
         <Text style={[styles.titles, styles.boxFrame, {margin: 10, marginBottom: 30}]}>Your address: {a}, {ci}, {co}</Text>
